02c84f8ad8431f84679d8cd756514ef20062cc6c
Kevin Wong <k.wong4@yahoo.com>
2020-08-05 00:08:47 -0400

    attributes.tf created. Outputs to terminal provided

---
+++ outputs/attributes.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+output "eip" {
+  value = aws_eip.lb.public_ip 
+}
+
+resource "aws_s3_bucket" "mys3" {
+  bucket = "kevins-terraform-s3"
+}
+
+output "mys3bucket" {
+  value = aws_s3_bucket.mys3.bucket_domain_name
+}
