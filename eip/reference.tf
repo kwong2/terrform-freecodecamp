175f6a3f89dcc3c613b595bbfffee202f68079dc
Kevin Wong <k.wong4@yahoo.com>
2020-08-05 22:23:57 -0400

    attempt to associate eip to newly created instance

---
+++ eip/reference.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-07c8bc5c1ce9598c3"
+    instance_type = "t2.micro"
+}
+
+resource "aws_eip" "one" {
+    vpc = true
+}
+
+resource "aws_eip_association" "myeip" {
+  instance_id = aws_instance.web.id 
+  public_ip = aws_eip.one.id
+}
