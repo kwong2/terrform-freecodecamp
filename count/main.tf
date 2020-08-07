21efa64c419bf5a758c9030f483e3539c7a264fb
Kevin Wong <k.wong4@yahoo.com>
2020-08-06 23:14:39 -0400

    added a variables.tf file and changed instance names with list index

--- count/main.tf
+++ count/main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami = "ami-07c8bc5c1ce9598c3"
   instance_type = "t2.micro"
-  count = 4
+  count = 3
   tags = {
-      Name = "Kev's terraform test.${count.index}"
+      Name = var.ec2_names[count.index]
   }
 }
---
+++ count/variables.tf
@@ -0,0 +1,4 @@
+variable "ec2_names" {
+    type = list
+    default = ["QA-test", "Stage-Test", "Prod-Test"]
+}
