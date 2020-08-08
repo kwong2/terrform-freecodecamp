6635e26e028105cbac776f9ee6d4ab5ad50a1bdf
Kevin Wong <k.wong4@yahoo.com>
2020-08-07 23:38:19 -0400

    added variable and count for conditional

--- conditional expression/conditionals.tf
+++ conditional expression/conditionals.tf
@@ -2,10 +2,13 @@ provider "aws"{
     region = "us-east-2"
 }
 
+variable "istest" {}
+
 resource "aws_instance" "dev" {
     ami =  "ami-07c8bc5c1ce9598c3"
     instance_type = "t2.micro"
-
+    
+    count = var.istest == true ? 1 : 0
     tags = {
         Name = "DEV"
     }
@@ -15,6 +18,7 @@ resource "aws_instance" "prod"{
     ami = " ami-07c8bc5c1ce9598c3"
     instance_type = "t2.medium"
 
+    count = var.istest == false ? 1 : 0
     tags = {
         Name = "PROD"
     }
