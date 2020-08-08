840509d4295a1517367a17734ca1b2c4c2d614ba
Kevin Wong <k.wong4@yahoo.com>
2020-08-08 00:27:23 -0400

    test to see if local variables can be referenced with same name in resource

--- local values/local.tf
+++ local values/local.tf
@@ -6,10 +6,12 @@ locals {
     common_names = {
         Name = "Kev's Testing"
     }
+    ami = "ami-07c8bc5c1ce9598c3"
+    instance_type = "t2.micro"
 }
 
 resource "aws_instance" "web" {
-    ami = "ami-07c8bc5c1ce9598c3"
-    instance_type = "t2.micro"
+    ami = local.ami
+    instance_type = local.instance_type
     tags = local.common_names
 }
