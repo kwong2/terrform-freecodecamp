e8a83b119786cb8792366912e2fa71c316494cd9
Kevin Wong <k.wong4@yahoo.com>
2020-08-20 23:38:34 -0400

    added iterator

--- dynamic/dynamic.tf
+++ dynamic/dynamic.tf
@@ -14,10 +14,11 @@ resource "aws_security_group" "dynamicsg" {
 
     dynamic "ingress" {
         for_each = var.sg_ports
+        iterator = port
 
         content {
-            from_port = ingress.value
-            to_port   = ingress.value
+            from_port = port.value
+            to_port   = port.value
             protocol  = "tcp"
             cidr_blocks = ["0.0.0.0/0"]
         }
