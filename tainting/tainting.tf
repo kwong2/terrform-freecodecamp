29aa7f54498cd1ce83008124e89fb86f9b620823
Kevin Wong <k.wong4@yahoo.com>
2020-08-22 22:32:34 -0400

    initial commit

---
+++ tainting/tainting.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "test1" {
+    ami = "ami-02354e95b39ca8dec"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Kevin_terraform_1"
+    }
+}
+
+resource "aws_instance" "test2"{
+    ami = "ami-02354e95b39ca8dec"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Kevin_terraform_2"
+    }
+}
