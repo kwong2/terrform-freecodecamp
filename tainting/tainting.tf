9b2ceb40ae33c6354b254de9345c36a4aac64f79
Kevin Wong <k.wong4@yahoo.com>
2020-08-22 22:42:36 -0400

    tainted test2 instance with command terraform taint aws_instance.test2

--- tainting/tainting.tf
+++ tainting/tainting.tf
@@ -11,6 +11,8 @@ resource "aws_instance" "test1" {
     }
 }
 
+#taint this resource
+#tainted with terraform taint aws_instance.test2
 resource "aws_instance" "test2"{
     ami = "ami-02354e95b39ca8dec"
     instance_type = "t2.micro"
