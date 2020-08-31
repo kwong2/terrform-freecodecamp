62d7654e9b36c5dfccb2e9e11f558485b73bcfa3
Kevin Wong <k.wong4@yahoo.com>
2020-08-30 22:48:46 -0400

    created sg

---
+++ provisioners/sg.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "chworkspace" {
+  name        = "chworkspace"
+  description = "Allow traffic from CH Workspaces"
+#   vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["52.2.120.210/32"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["52.2.120.210/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
