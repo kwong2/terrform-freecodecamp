ec56122678c1de4d3917aae4a39f60eb6d6ddbe6
Kevin Wong <k.wong4@yahoo.com>
2020-08-31 23:16:44 -0400

    added new instance to install docker

--- provisioners/remote-exec.tf
+++ provisioners/remote-exec.tf
@@ -28,3 +28,37 @@ resource "aws_eip" "lb" {
   instance = aws_instance.test.id
   vpc      = true
 }
+
+
+resource "aws_instance" "docker" {
+    ami = "ami-02354e95b39ca8dec"
+    instance_type = "t2.micro"
+    key_name = "KevinTerraform"
+
+    security_groups = ["chworkspace"]
+
+    tags = {
+        Name = "Kevin_terraform_2"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum update -y",
+            "sudo amazon-linux-extras install -y docker",
+            "sudo service docker start",
+            "sudo usermod -a -G docker ec2-user"
+        ]
+        connection {
+            type = "ssh"
+            user = "ec2-user"
+            private_key = file("./KevinTerraform.pem")
+            host = self.public_ip
+        }
+
+    }
+}
+
+resource "aws_eip" "docker-eip" {
+  instance = aws_instance.docker.id
+  vpc      = true
+}
