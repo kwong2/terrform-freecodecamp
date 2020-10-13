bc8c0c5c7e3c57a81f9a9792d398e5498604a1b2
Kevin Wong <k.wong4@yahoo.com>
2020-10-13 16:33:09 -0400

    added dynamic iterator for egress security group

--- dynamic/dynamic.tf
+++ dynamic/dynamic.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-west-1"
 }
 
 variable "sg_ports" {
@@ -24,4 +24,18 @@ resource "aws_security_group" "dynamicsg" {
         }
     }
 
+    dynamic "egress"{
+        for_each = var.sg_ports
+        iterator = port
+
+        content {
+            from_port = port.value
+            to_port = port.value
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
+    tags = {
+      "Name" = "TestDynamic"
+    }
 }
